bb07c67f820c9b074ea0233b05b003e61343195c
goku-ts <hgb.mega@gmail.com>
2024-01-20 14:37:46 -0800

    new

---
+++ file-upload/upload-server.js
@@ -0,0 +1,35 @@
+import http from "node:http"
+import fs from "node:fs/promises"
+import { pipeline } from "node:stream"
+
+const server = http.createServer()
+
+server.on("request", async (request, response) => {
+
+    const index = request.rawHeaders.indexOf("Content-Type") + 1
+    const subIndex = request.rawHeaders[index].indexOf("/")
+    let extention = request.rawHeaders[index].substring(subIndex + 1)
+
+    const genNumber = Math.trunc(Math.random() * 999) + 1
+
+    const fileHandle = await fs.open(`storage/${genNumber}.${extention}`, "w")
+    const writeFile = fileHandle.createWriteStream()
+
+    pipeline(request, writeFile, (err) => {
+        if (err) {
+            response.end(
+                JSON.stringify({ message: "problem uploading file.. try again" })
+            )
+        }
+    })
+
+    writeFile.on("finish", () => {
+        response.end(
+            JSON.stringify({ message: "file uploaded successfully" })
+        )
+
+    })
+})
+
+
+server.listen(5050, "localhost", () => console.log("server running"))
